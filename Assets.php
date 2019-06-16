3c50a322f36a1f261217767b36b47c56932d70a7
a.shahimian <shahimian@gmail.com>
2019-06-16 15:56:33 +0430

    initial project

---
+++ Assets.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shahimian
+ * Date: 7/7/2016
+ * Time: 9:26 PM
+ */
+
+namespace app\assets\iransans;
+
+
+use yii\web\AssetBundle;
+
+class Assets extends AssetBundle
+{
+
+    public $sourcePath = "@app/assets/iransans/font";
+
+    public $css = [
+        'font.css',
+    ];
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+use this class to any page following below:
+use app\assets\iransans\Assets as Iransans;
+and loading this one:
+Iransans::register($this);
+Any property of css you want to assign to font-family of Iransans
